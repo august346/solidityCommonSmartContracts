c950ff0bbd29ce3acec4f4e59f206c31a3c50190
august346 <foo@bar.baz>
2024-08-29 13:35:11 +0800

    fix - typo

--- MyVoter.sol
+++ MyVoter.sol
@@ -2,14 +2,45 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-contract MyStorage {
-    string _data;
-
-    function setData(string memory newData) public {
-        _data = newData;
+contract Voting {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
     }
 
-    function getData() public view returns (string memory) {
-        return _data;
+    struct Voter {
+        bool voted;
+        uint vote;
+    }
+
+    mapping(address => Voter) public voters;
+    Candidate[] public candidates;
+
+    event Voted(address indexed voter, uint indexed candidateId);
+
+    function addCandidate(string memory name) public {
+        candidates.push(Candidate(candidates.length, name, 0));
+    }
+
+    modifier onlyOnce() {
+        require(!voters[msg.sender].voted, "You have already voted.");
+        _;
+    }
+
+    function vote(uint candidateId) public onlyOnce {
+        require(candidateId < candidates.length, "Invalid candidate ID.");
+
+        voters[msg.sender].voted = true;
+        voters[msg.sender].vote = candidateId;
+
+        candidates[candidateId].voteCount++;
+
+        emit Voted(msg.sender, candidateId);
+    }
+
+    function getCanddidates() public view returns (Candidate[] memory) {
+        return candidates;
     }
 }
+
